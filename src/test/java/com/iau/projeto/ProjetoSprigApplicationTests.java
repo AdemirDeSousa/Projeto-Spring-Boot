41a6bf3522ac43967a2de1176159b9bfdd053f26
AdemirDeSousa <jadsqj@gmail.com>
2019-11-28 15:56:09 -0300

    Projeto criado

---
+++ src/test/java/com/iau/projeto/ProjetoSprigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iau.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSprigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
