838dc71ace64dc04b8657b5ce865f93b7ec907d3
AdemirDeSousa <jadsqj@gmail.com>
2019-11-30 10:35:47 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/iau/projeto/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.iau.projeto.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iau.projeto.domain.Cliente;
+import com.iau.projeto.repositories.ClienteRepository;
+import com.iau.projeto.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	//Instanciando dependencia pelo mecanismo de injeçao
+	@Autowired
+	//Objeto do tipo ClienteRepository
+	private ClienteRepository categoria_repository;
+	
+	public Cliente Find(Integer id) {
+		Optional<Cliente> obj = categoria_repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		}
+}
