2a894bb334662ac62c036f7c26b1238a71411b1b
AdemirDeSousa <jadsqj@gmail.com>
2019-11-29 22:01:36 -0300

    Criando operacao de instanciacao

--- src/main/java/com/iau/projeto/ProjetoSprigApplication.java
+++ src/main/java/com/iau/projeto/ProjetoSprigApplication.java
@@ -1,13 +1,34 @@
 package com.iau.projeto;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjetoSprigApplication {
+import com.iau.projeto.domain.Categoria;
+import com.iau.projeto.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class ProjetoSprigApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoria_repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSprigApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		//Objetos
+		Categoria cat_1 = new Categoria(null, "Informatica");
+		Categoria cat_2 = new Categoria(null, "Escritorio");
+		
+		//Salvar objetos no banco de dados
+		categoria_repository.saveAll(Arrays.asList(cat_1, cat_2));
 	}
 
 }
